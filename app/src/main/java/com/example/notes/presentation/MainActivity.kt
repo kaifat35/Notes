cfbed1bedfa41d7613be1b28fc9adb33d46705dc
Алексей <mr-kaifat@yandex.ru>
2025-06-09 22:35:14 +0300

    добавлено NavGraph

--- app/src/main/java/com/example/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notes/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.notes.presentation.navigation.NavGraph
 import com.example.notes.presentation.screens.creation.CreateNoteScreen
 import com.example.notes.presentation.screens.editing.EditNoteScreen
 import com.example.notes.presentation.screens.notes.NotesScreen
@@ -16,25 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotesTheme {
-                EditNoteScreen(
-                    noteId = 5,
-                    onFinished = {
-                       Log.d("EditeNoteScreen", "Finished")
-                   }
-                )
-//                CreateNoteScreen(
-//                    onFinished = {
-//                        Log.d("CreateNoteScreen", "Finished")
-//                    }
-//                )
-//                NotesScreen(
-//                    onNoteClick = {
-//                        Log.d("MainActivity", "onNoteClick: $it")
-//                    },
-//                    onAddNoteClick = {
-//                        Log.d("MainActivity", "onAddNoteClick")
-//                    }
-//                )
+                NavGraph()
             }
         }
     }
