3231b15b46fd16dd04903ba82915b67f7be895e3
Алексей <mr-kaifat@yandex.ru>
2025-06-14 11:12:25 +0300

    добавлены NotesDao, NoteDbModel, Mapper

---
+++ app/src/main/java/com/example/notes/data/Mapper.kt
@@ -0,0 +1,11 @@
+package com.example.notes.data
+
+import com.example.notes.domain.Note
+
+fun Note.toDbModel(): NoteDbModel {
+    return NoteDbModel(id, title, content, updatedAt, isPinned)
+}
+
+fun NoteDbModel.toEntity(): Note {
+    return Note(id, title, content, updatedAt, isPinned)
+}
